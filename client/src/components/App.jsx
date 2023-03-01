bef221a058c401436d9666fc39a54c44f85e0de7
Michael Arita <michael.arita01@gmail.com>
2023-03-01 13:38:31 -0800

    chore:routine push

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { Routes, Route } from 'react-router';
+import { Switch, Route } from 'react-router';
+
 
 
 class App extends Component {
   render() {
     return(
-      <div>
+      <div className='router'>
         <h1> Hello world! Testing Testing </h1>
         <a href='/spotify' className='btn btn-primary'>Log in with Spotify</a>
         <a href='/spotify/users-liked-songs' className='test'>Next page</a>
---
+++ client/src/components/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+  return(
+    <>
+      <h1> Hello world! Testing Testing </h1>
+      <a href='/spotify' className='btn btn-primary'>Log in with Spotify</a>
+      <a href='/spotify/users-liked-songs' className='test'>Next page</a>
+    </>
+  );
+};
