28f66061d45162289fd065d4b027cad3d76dc3e8
Michael Arita <michael.arita01@gmail.com>
2023-03-02 18:15:28 -0800

    chore: default commit

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App.jsx';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <App />,
